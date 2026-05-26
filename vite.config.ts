9d8b970c3db7adfd5d9d94ef34d0fd7ffe65d033
glaack <aj1239@nyu.edu>
2026-05-26 11:47:48 -0400

    Change base path for portfolio website

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/PortfolioWebsite/',
+  base: '/Casey-Judge-Portfolio/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
